32556f9b1b8ef33b28347ab56a17ccd543afc369
c-blaq <babayejuadesina@gmail.com>
2024-12-17 20:30:51 +0100

    feat: disable vc name selection & increase divider width

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,46 @@
 <template>
-  
   <div class="hello container" id="app">
     <h1>{{ msg }}</h1>
 
     <input type="file" @change="uploadPhoto" />
     <div class="inputs">
-      <input type="text" placeholder="Firstname Lastname Surname" v-model="details.name" class="input" @input="updateTextOverlay" />
-      <input type="text" placeholder="Course" v-model="details.course" class="input" @input="updateTextOverlay" />
+      <input
+        type="text"
+        placeholder="Firstname Lastname Surname"
+        v-model="details.name"
+        class="input"
+        @input="updateTextOverlay"
+      />
+      <input
+        type="text"
+        placeholder="Course"
+        v-model="details.course"
+        class="input"
+        @input="updateTextOverlay"
+      />
     </div>
     <!-- <canvas ref="canvas" width="500" height="500" class="canvas"></canvas> -->
-    <canvas ref="canvas" width="500" height="500" style="width: 500px; height: 500px;" class="canvas"></canvas>
+    <canvas
+      ref="canvas"
+      width="500"
+      height="500"
+      style="width: 500px; height: 500px"
+      class="canvas"
+    ></canvas>
 
     <button @click="downloadImage" class="button">Download Photo</button>
 
     <h3>Essential Links</h3>
     <ul>
       <!-- <li><a href="https://www.linkedin.com/in/jekayinoluwa/" target="_blank" rel="noopener">Say hi to me</a></li> -->
-      <li><a href="https://gdg.community.dev/gdg-on-campus-kwara-state-university-malete-nigeria/" target="_blank" rel="noopener">Join GDGoC KWASU</a></li>
+      <li>
+        <a
+          href="https://gdg.community.dev/gdg-on-campus-kwara-state-university-malete-nigeria/"
+          target="_blank"
+          rel="noopener"
+          >Join GDGoC KWASU</a
+        >
+      </li>
     </ul>
   </div>
 </template>
@@ -25,9 +49,9 @@
 import { fabric } from "fabric";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
@@ -35,7 +59,7 @@ export default {
         name: "",
         course: "",
       },
-      nameText: null,  
+      nameText: null,
       courseText: null,
       canvas: null,
       staticText: {
@@ -43,7 +67,7 @@ export default {
         professorName: "Professor Shaykh-Luqman Alade Jimoh",
         viceChancellor: "VICE-CHANCELLOR",
       },
-      bannerText: null, 
+      bannerText: null,
     };
   },
 
@@ -71,18 +95,18 @@ export default {
           this.addStaticText();
           this.addLineDivider();
         });
-      }
+      };
       reader.readAsDataURL(file);
     },
 
     addLogo() {
-      fabric.Image.fromURL(require('@/assets/logo.png'), (logo) => {
-        logo.scaleToWidth(50); 
+      fabric.Image.fromURL(require("@/assets/logo.png"), (logo) => {
+        logo.scaleToWidth(50);
         logo.set({
-          left: 10, 
+          left: 10,
           top: 10,
-          opacity: 0.9,  
-          selectable: false
+          opacity: 0.9,
+          selectable: false,
         });
         this.canvas.add(logo);
         this.canvas.renderAll();
@@ -91,59 +115,57 @@ export default {
 
     addLogoText() {
       const logoText = new fabric.Textbox("KWARA STATE UNIVERSITY\nNIGERIA", {
-        left: 56,  
-        top: 14, 
+        left: 56,
+        top: 14,
         fontSize: 12,
-        fill: 'white',
+        fill: "white",
         // backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        stroke: 'black', 
+        stroke: "black",
         strokeWidth: 0.6,
-        charSpacing: 1, 
+        charSpacing: 1,
         opacity: 0.98,
-        fontWeight: 'bold',
-        fontFamily: 'Arial',
-        lineHeight: 1.2, 
-        width: 200, 
-        selectable: false
+        fontWeight: "bold",
+        fontFamily: "Arial",
+        lineHeight: 1.2,
+        width: 200,
+        selectable: false,
       });
 
       this.canvas.add(logoText);
       this.canvas.renderAll();
     },
 
-
     addBanner() {
       const dynamicText = new fabric.Text(
         `${this.details.name || "Full Name"}`,
-        `${this.details.course || "Course"}`,
+        `${this.details.course || "Course"}`
       );
-    
+
       const textHeight = dynamicText.getBoundingRect().height;
       const bannerHeight = textHeight + 70;
       const bannerGradient = new fabric.Gradient({
-      type: 'linear',
-      gradientUnits: 'objectBoundingBox',
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 0,
-        y2: 1, // vertical gradient
-      },
-      colorStops: [
-        { offset: 0, color: 'rgba(1, 100, 61, 0.4)' },
-        { offset: 0.5, color: 'rgba(1, 121, 111, 0.3)' }, 
-        { offset: 1, color: 'rgba(0, 75, 81, 0.7)' },  
-      ],
+        type: "linear",
+        gradientUnits: "objectBoundingBox",
+        coords: {
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1, // vertical gradient
+        },
+        colorStops: [
+          { offset: 0, color: "rgba(1, 100, 61, 0.4)" },
+          { offset: 0.5, color: "rgba(1, 121, 111, 0.3)" },
+          { offset: 1, color: "rgba(0, 75, 81, 0.7)" },
+        ],
       });
-
 
       const banner = new fabric.Rect({
         left: 0,
-        top: this.canvas.height - bannerHeight, 
+        top: this.canvas.height - bannerHeight,
         // fill: "#01796f",
         fill: bannerGradient,
         width: this.canvas.width,
-        height: bannerHeight, 
+        height: bannerHeight,
         selectable: false,
       });
       this.canvas.add(banner);
@@ -153,133 +175,130 @@ export default {
 
     addTextOverlay(bannerHeight) {
       if (!this.nameText || !this.courseText) {
-        this.nameText = new fabric.Textbox(
-          this.details.name || "Full Name",  
-          {
-            left: this.canvas.width / 2,
-            top: this.canvas.height - bannerHeight + 10, 
-            width: this.canvas.width - 40,  
-            textAlign: "center",
-            originX: "center",
-            fill: "white",
-            fontSize: 16,
-            fontWeight: "bold",
-            fontFamily: 'Merriweather, serif', 
-            selectable: false,
-            lineHeight: 1.2, 
-          }
-        );
+        this.nameText = new fabric.Textbox(this.details.name || "Full Name", {
+          left: this.canvas.width / 2,
+          top: this.canvas.height - bannerHeight + 10,
+          width: this.canvas.width - 40,
+          textAlign: "center",
+          originX: "center",
+          fill: "white",
+          fontSize: 16,
+          fontWeight: "bold",
+          fontFamily: "Merriweather, serif",
+          selectable: false,
+          lineHeight: 1.2,
+        });
         this.canvas.add(this.nameText);
 
-        this.courseText = new fabric.Textbox(
-          this.details.course || "Course",  
-          {
-            left: this.canvas.width / 2,
-            top: this.nameText.top + this.nameText.height + 5,
-            width: this.canvas.width - 40,  
-            textAlign: "center",
-            originX: "center",
-            fill: "white",
-            opacity: 0.8,
-            fontSize: 12,
-            fontWeight: "normal",
-            fontFamily: 'Merriweather, serif',
-            selectable: false,
-            lineHeight: 1.2, 
-          }
-        );
+        this.courseText = new fabric.Textbox(this.details.course || "Course", {
+          left: this.canvas.width / 2,
+          top: this.nameText.top + this.nameText.height + 5,
+          width: this.canvas.width - 40,
+          textAlign: "center",
+          originX: "center",
+          fill: "white",
+          opacity: 0.8,
+          fontSize: 12,
+          fontWeight: "normal",
+          fontFamily: "Merriweather, serif",
+          selectable: false,
+          lineHeight: 1.2,
+        });
         this.canvas.add(this.courseText);
       }
     },
 
-
     addLineDivider() {
-      const divider = new fabric.Line([
-        this.canvas.width / 4, this.canvas.height - 60, 
-        3 * this.canvas.width / 4, this.canvas.height - 60
-      ], {
-        stroke: "#ffffff",      
-        strokeWidth: 2,      
-        selectable: false,   
-      
-      });
+      const divider = new fabric.Line(
+        [
+          this.canvas.width / 4.5,
+          this.canvas.height - 60,
+          (3 * this.canvas.width) / 4,
+          this.canvas.height - 60,
+        ],
+        {
+          stroke: "#ffffff",
+          strokeWidth: 2,
+          selectable: false,
+        }
+      );
       this.canvas.add(divider);
     },
 
     addStaticText() {
-    const { classYear, professorName, viceChancellor } = this.staticText;
+      const { classYear, professorName, viceChancellor } = this.staticText;
 
-    const offset = 10;  
+      const offset = 10;
 
-    const classText = new fabric.Text(classYear, {
-      left: this.canvas.width / 2,
-      top: this.canvas.height - offset - 45,  
-      textAlign: "center",
-      originX: "center",
-      fill: "white",
-      fontSize: 14,
-      fontWeight: "bold"
-    });
-    this.canvas.add(classText);
+      const classText = new fabric.Text(classYear, {
+        left: this.canvas.width / 2,
+        top: this.canvas.height - offset - 45,
+        textAlign: "center",
+        originX: "center",
+        fill: "white",
+        fontSize: 14,
+        fontWeight: "bold",
+        selectable: false,
+      });
+      this.canvas.add(classText);
 
-    const professorText = new fabric.Text(professorName, {
-      left: this.canvas.width / 2,
-      top: this.canvas.height - offset - 26, 
-      textAlign: "center",
-      originX: "center",
-      fill: "white",
-      opacity: 0.9,
-      fontFamily: 'Lobster, serif', 
-      fontSize: 14,
-      fontWeight: "bold"
-    });
-    this.canvas.add(professorText);
+      const professorText = new fabric.Text(professorName, {
+        left: this.canvas.width / 2,
+        top: this.canvas.height - offset - 26,
+        textAlign: "center",
+        originX: "center",
+        fill: "white",
+        opacity: 0.9,
+        fontFamily: "Lobster, serif",
+        fontSize: 14,
+        fontWeight: "bold",
+        selectable: false,
+      });
+      this.canvas.add(professorText);
 
-    const viceChancellorText = new fabric.Text(viceChancellor, {
-      left: this.canvas.width / 2,
-      top: this.canvas.height - offset - 8,  
-      textAlign: "center",
-      originX: "center",
-      fill: "white",
-      opacity: 0.7,
-      fontSize: 8,
-      fontWeight: "bold"
-    });
-    this.canvas.add(viceChancellorText);
+      const viceChancellorText = new fabric.Text(viceChancellor, {
+        left: this.canvas.width / 2,
+        top: this.canvas.height - offset - 8,
+        textAlign: "center",
+        originX: "center",
+        fill: "white",
+        opacity: 0.7,
+        fontSize: 8,
+        fontWeight: "bold",
+      });
+      this.canvas.add(viceChancellorText);
     },
 
     updateTextOverlay() {
       if (this.nameText) {
         this.nameText.set({
-          text: this.details.name || "Full Name"
+          text: this.details.name || "Full Name",
         });
 
         if (this.courseText) {
           this.courseText.set({
             text: this.details.course || "Course",
-            top: this.nameText.top + this.nameText.height + 5,  
+            top: this.nameText.top + this.nameText.height + 5,
           });
         }
-        this.canvas.renderAll();  
+        this.canvas.renderAll();
       }
     },
 
     downloadImage() {
       if (this.canvas) {
-        const scale = 2; 
+        const scale = 2;
         const tempCanvas = this.canvas.toDataURL({
           format: "png",
-          multiplier: scale, 
+          multiplier: scale,
         });
         const link = document.createElement("a");
         link.download = "graduation-banner.png";
         link.href = tempCanvas;
         link.click();
       }
-      
     },
-  }
-
+  },
 };
 </script>
 
@@ -304,8 +323,8 @@ a {
   margin: auto;
   display: flex;
   flex-direction: column;
-  align-items: center; 
-  margin: 0 auto; 
+  align-items: center;
+  margin: 0 auto;
   text-align: center;
 }
 .canvas {
@@ -316,16 +335,16 @@ a {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%; 
-  max-width: 400px; 
+  width: 100%;
+  max-width: 400px;
 }
 .input {
   display: block;
-  margin: 10px 0; 
+  margin: 10px 0;
   padding: 10px;
   width: 100%;
-  max-width: 100%; 
-  box-sizing: border-box; 
+  max-width: 100%;
+  box-sizing: border-box;
 }
 .canvas {
   border: 1px solid #ccc;
